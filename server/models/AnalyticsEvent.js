2d79dde885f2720f3fea4e5fdcb4d30c33bffff2
ceidmo <moniqueceide@yahoo.com>
2025-06-07 20:07:28 -0400

    Update AnalyticsEvent.js

--- server/models/AnalyticsEvent.js
+++ server/models/AnalyticsEvent.js
@@ -4,14 +4,21 @@ const AnalyticsEventSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    index: true, // Index for fast filtering by event name
   },
   properties: {
     type: Object,
     default: {},
   },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    index: true, // Index for fast filtering by user
+  },
   timestamp: {
     type: Date,
     default: Date.now,
+    index: true, // Index for date range queries
   },
 });
 
